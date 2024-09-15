17648b85033ea910e85896fa3147c9f6da946ff4
Emiliano <emiliano.cabello7@iems.edu.mx>
2024-09-15 14:27:23 -0600

    feat: :art: Add the constructor object which will create the todo objects
    And I've created all Todos Factory object's method's templates, which will save the Todo's properties, for example, The Todo's Title

--- src/Todos.js
+++ src/Todos.js
@@ -1,7 +1,27 @@
-// class Todos {
-//    title;
-//    description;
-//    dueDate;
-//    priority;
-//    checklist;
-// }
+ function TodosFactory () {
+   this.title = title;
+   this.description = description;
+   this.dueDate = dueDate;
+   this.priority = priority;
+   this.checklist = checklist;
+}
+
+TodosFactory.prototype.addTodoTitle = function (title) {
+    this.title = title.toString();
+};
+
+TodosFactory.prototype.addDescription = function(description) {
+    return description.toString();
+};
+
+TodosFactory.prototype.addDate = function (dueDate) {
+    return dueDate;
+};
+
+TodosFactory.prototype.addPriority = function (priority) {
+    return priority;
+};
+
+TodosFactory.prototype.addChecklist = function (checklist) {
+    return checklist;
+};
