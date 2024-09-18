4fb5b813c3a5fceb6d51f4ee7b15c3d95d560754
Emiliano <emiliano.cabello7@iems.edu.mx>
2024-09-17 21:54:13 -0600

    feat:Add the storage which will save sections,Todo
    There are three methods, save, look and delete. in
    further steps I'll add a new parse Json format
    method

--- src/storage.js
+++ src/storage.js
@@ -1,39 +1,41 @@
-// function TodoListStorage() {
-//     { return SavesItems, LookUpStorage, RemoveItem};
-// };
+export {TodoListStorage};
 
-// const SavesItems = (key, item) => {
-//     if (storageAvailable('localStorage')) {
-//         localStorage.setItem(key, item);        
-//     } else
-//         console.log("I'm afraid that your browser doesn't support LocalStorage or your space has been filled");
-// } 
+function TodoListStorage() {
+    { return SavesItems, LookUpStorage, RemoveItem};
+};
 
-// const LookUpStorage = (key) => {
-//     if (localStorage.getItem(key)) {
-//         console.log('Item successfully added at the Storage')
-//     }else
-//     console.log(`the ${key} item not exist`)
-// };
+const SavesItems = (key, item) => {
+    if (storageAvailable('localStorage')) {
+        localStorage.setItem(key, item);        
+    } else
+        console.log("I'm afraid that your browser doesn't support LocalStorage or your space has been filled");
+} 
 
-// const RemoveItem = (key) => localStorage.removeItem(key);
+const LookUpStorage = (key) => {
+    if (localStorage.getItem(key)) {
+        console.log('Item successfully added at the Storage')
+    }else
+    console.log(`the ${key} item not exist`)
+};
 
-// function storageAvailable(type) {
-//     let storage;
-//     try {
-//       storage = window[type];
-//       const x = "__storage_test__";
-//       storage.setItem(x, x);
-//       storage.removeItem(x);
-//       return true;
-//     } catch (e) {
-//       return (
-//         e instanceof DOMException &&
-//         e.name === "QuotaExceededError" &&
-//         // acknowledge QuotaExceededError only if there's something already stored
-//         storage &&
-//         storage.length !== 0
-//       );
-//     }
-//   }
+const RemoveItem = (key) => localStorage.removeItem(key);
+
+function storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
   
